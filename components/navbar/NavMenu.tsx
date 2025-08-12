5c6d83e9431e3c8c0102bc56a8280cb92eda7917
pikachupradeep <nmura1000@gmail.com>
2025-08-12 17:29:32 +0530

    uiuyefg

--- components/navbar/NavMenu.tsx
+++ components/navbar/NavMenu.tsx
@@ -54,7 +54,7 @@ const handleClick = (): void => {
 
 
         <MenuItem setActive={setActive} active={active} item="Tours">
-          <div className="text-sm grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 p-4">
+          <div className="text-sm grid grid-cols-1 sm:grid-cols-2 gap-10 p-4">
             <ProductItem
               title="Pilgrimage Tours"
               href="https://algochurn.com"
